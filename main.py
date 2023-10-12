a636398dd5cdcbf6a61d8faa664c970d9e0d4a06
Nirigoyen <116807507+Nirigoyen@users.noreply.github.com>
2023-10-11 23:54:06 -0300

    Limpieza de comentarios

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 import random
 
 # ROTACIONES
@@ -10,6 +11,9 @@ def rotateCube(cube, direction): # 1 = derecha, 2 = izquierda, 3 = arriba, 4 = a
         cube.front, cube.top, cube.left, cube.bottom = cube.bottom, cube.front, cube.top, cube.left
     elif direction == 4:
         cube.front, cube.top, cube.left, cube.bottom = cube.top, cube.left, cube.bottom, cube.front
+
+def rotateFace:
+
 
 def rotateRow(cube, row, direction): # 1 = derecha, 2 = izquierda
     if direction == 1:
@@ -29,6 +33,28 @@ def scramble(cube):
         rotateCol(cube, random.randint(1, 3), random.randint(3, 4))
         rotateCube(cube, random.randint(1, 4))
         rotateRow(cube, random.randint(1, 3), random.randint(1, 2))
+
+def positionFaces(cube):
+    copycube = [copy.deepcopy(cube.front), copy.deepcopy(cube.right), copy.deepcopy(cube.left), copy.deepcopy(cube.back), copy.deepcopy(cube.top), copy.deepcopy(cube.bottom)]
+
+    for i in range(len(copycube)):
+        if copycube[i][1][1] == 1:
+            cube.front = copycube[i]
+        elif copycube[i][1][1] == 2:
+            cube.right = copycube[i]
+        elif copycube[i][1][1] == 3:
+            cube.left = copycube[i]
+        elif copycube[i][1][1] == 4:
+            cube.back = copycube[i]
+        elif copycube[i][1][1] == 5:
+            cube.top = copycube[i]
+        elif copycube[i][1][1] == 6:
+            cube.bottom = copycube[i]
+def solve(cube):
+    copyFaces = [copy.deepcopy(cube.front), copy.deepcopy(cube.right), copy.deepcopy(cube.left), copy.deepcopy(cube.back), copy.deepcopy(cube.top), copy.deepcopy(cube.bottom)]
+    positionFaces(cube)
+
+
 
 
 
@@ -50,6 +76,10 @@ def printCube(cube):
     printFace(cube.left)
     print()
 
+    print("atras")
+    printFace(cube.back)
+    print()
+
     print("arriba")
     printFace(cube.top)
     print()
@@ -61,29 +91,38 @@ def printCube(cube):
 
 if __name__ == '__main__':
     class cube:
-        front = [[1, 1, 1],
+        front = [[1, 1, 1], # ROJO
                  [1, 1, 1],
                  [1, 1, 1]]
 
-        right = [[2, 2, 2],
+        right = [[2, 2, 2], # VERDE
                  [2, 2, 2],
                  [2, 2, 2]]
 
-        left =  [[3, 3, 3],
+        left =  [[3, 3, 3], # AZUL
                  [3, 3, 3],
                  [3, 3, 3]]
 
-        back =  [[4, 4, 4],
+        back =  [[4, 4, 4], # NARANJA
                  [4, 4, 4],
                  [4, 4, 4]]
 
-        top =   [[5, 5, 5],
+        top =   [[5, 5, 5], # AMARILLO
                  [5, 5, 5],
                  [5, 5, 5]]
 
-        bottom =[[6, 6, 6],
+        bottom =[[6, 6, 6], # BLANCO
                  [6, 6, 6],
                  [6, 6, 6]]
+
+
+    printCube(cube)
+
+    rotateCol(cube, 1, 3)
+
+    printCube(cube)
+
+    #FALTA HACER QUE CUANDO SE ROTE UNA FILA O COLUMNA QUE SEA DEL BORDE, SE GIRE LA CARA TAMBIEN
 
 
 
